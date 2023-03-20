9021d47c066c007849f9256c6348ccd109270fa9
abdulhamidongithub <1997abdulhamid@gmail.com>
2023-03-20 16:25:34 +0300

    Swagger docs added

--- FarStroy/urls.py
+++ FarStroy/urls.py
@@ -3,6 +3,20 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from asosiy.views import *
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="FarStroyGroup web-API",
+      default_version='v1',
+      description="FarStroyGroup korxonasi web-dasturi uchun qurilgan web-API",
+      contact=openapi.Contact(email="1997abdulhamid@gmail.com"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +25,6 @@ urlpatterns = [
     path('kataloglar/', KataloglarAPIView.as_view()),
     path('fasadlar/', FasadlarAPIView.as_view()),
     path('bizning_uylar/', BizningUylarAPIView.as_view()),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
