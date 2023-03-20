39a415a6e510f0c36a9bc8cd61d62df3eb492a09
abdulhamidongithub <1997abdulhamid@gmail.com>
2023-03-20 16:03:04 +0300

    Views finished

--- FarStroy/urls.py
+++ FarStroy/urls.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from asosiy.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('asosiy/', AsosiyAPIView.as_view()),
+    path('karyusellar/', KaryusellarAPIView.as_view()),
+    path('kataloglar/', KataloglarAPIView.as_view()),
+    path('fasadlar/', FasadlarAPIView.as_view()),
+    path('bizning_uylar/', BizningUylarAPIView.as_view()),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- asosiy/views.py
+++ asosiy/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import *
+from .serializers import *
 
+class AsosiyAPIView(APIView):
+    def get(self, request):
+        serializer = AsosiySerializer(Asosiy.objects.last())
+        return Response(serializer.data)
+
+class KaryusellarAPIView(APIView):
+    def get(self, request):
+        serializer = KaryuselSerializer(KaryuselRasm.objects.all(), many=True)
+        return Response(serializer.data)
+
+class KataloglarAPIView(APIView):
+    def get(self, request):
+        serializer = KatalogSerializer(Katalog.objects.all(), many=True)
+        return Response(serializer.data)
+
+class FasadlarAPIView(APIView):
+    def get(self, request):
+        serializer = FasadSerializer(Fasad.objects.all(), many=True)
+        return Response(serializer.data)
+
+class BizningUylarAPIView(APIView):
+    def get(self, request):
+        serializer = BizningUylarSerializer(BizningUylar.objects.all(), many=True)
+        return Response(serializer.data)
 
